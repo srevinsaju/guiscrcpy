2ba66a918f42c613d9b03b72053c3387c0e61c23
srevinsaju <srevin03@gmail.com>
2019-06-13 15:45:12 +0300

    initial build

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
 import sys
 from PyQt4 import QtCore, QtGui, uic
 
-qtCreatorFile = "mainwindow.ui" # Enter file here.
+qtCreatorFile = "mainwindow.ui"  # Enter file here.
 
 Ui_MainWindow, QtBaseClass = uic.loadUiType(qtCreatorFile)
+
 
 class MyApp(QtGui.QMainWindow, Ui_MainWindow):
     def __init__(self):
         QtGui.QMainWindow.__init__(self)
         Ui_MainWindow.__init__(self)
         self.setupUi(self)
+
 
 if __name__ == "__main__":
     app = QtGui.QApplication(sys.argv)

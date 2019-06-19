081610723d0baf12ff840144a1bb677868ca0a16
srevinsaju <srevin03@gmail.com>
2019-06-19 14:51:06 +0300

    build 2.1

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # GUIscrcPy
+A full fledged GUI integration for the most award winning open-source android screen mirroring system -- `scrcpy` located on `https://github.com/genymobile/scrcpy/`
+
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+#
 """
 GUISCRCPY by srevinsaju
 Get it on : https://github.com/sevinsaju/guiscrcpy
@@ -8,22 +9,74 @@ import sys
 
 from PyQt4 import QtGui, uic
 
+build = 2.0
 qtCreatorFile = "mainwindow.ui"  # Enter file here.
-tmp = []
+
 Ui_MainWindow, QtBaseClass = uic.loadUiType(qtCreatorFile)
 
 
 class MyApp(QtGui.QMainWindow, Ui_MainWindow):
     def __init__(self):
         tmp = ""
+        dimension = None
+        bit_rate = 8000  # default
+
         QtGui.QMainWindow.__init__(self)
         Ui_MainWindow.__init__(self)
         self.setupUi(self)
+
+        # CONNECT DIMENSION CHECK BOX TO STATE CHANGE
+        self.dimensionDefaultCheckbox.stateChanged.connect(self.dimensionChange)
+
+        # CHECK BOX GROUP CONNECT
+        self.
+
+        # DIAL CTRL GRP
+        self.dial.sliderMoved.connect(self.dial_text_refresh)
+        self.dial.sliderReleased.connect(self.dial_text_refresh)
+        # DIAL CTRL GRP
+
+        # MAIN EXECUTE ACTION
         self.executeaction.clicked.connect(self.start_act)
 
-    def start_act(self, ):
+    def dimensionChange(self):
+
+        if self.dimensionDefaultCheckbox.isChecked():
+            self.dimensionSlider.setEnabled(False)
+            dimension = None
+            self.dimensionText.setText("DEFAULT")
+
+        else:
+            self.dimensionSlider.setEnabled(True)
+            dimension = int(self.dimensionSlider.value())
+            self.dimensionText.setText(str(dimension) + "px")
+            self.dimensionSlider.sliderMoved.connect(self.slider_text_refresh)
+            self.dimensionSlider.sliderReleased.connect(self.slider_text_refresh)
+
+    def slider_text_refresh(self):
+        dimension = int(self.dimensionSlider.value())
+        self.dimensionText.setText(str(dimension) + "px")
+        pass
+
+    def dial_text_refresh(self):
         bit_rate = int(self.dial.value())
-        tax = (self.dimensionSlider.value())
+        self.bitrateText.setText(str(bit_rate) + "KB/s")
+        pass
+
+    def start_act(self):
+
+        # check if the defaultDimension is checked or not for giving signal
+        if self.dimensionDefaultCheckbox.isChecked():
+            self.dimensionSlider.setEnabled(False)
+            self.dimensionText.setText("DEFAULT")
+            dimension = None
+
+        else:
+            self.dimensionSlider.setEnabled(True)
+            dimension = int(self.dimensionSlider.value())
+            self.dimensionText.setText(str(dimension) + "px")
+        # check if the defaultDimension is checked or not for giving signal
+
         """
         proc = subprocess.run(["scrcpy"], stdout=subprocess.PIPE,
                                 stderr=subprocess.PIPE)
@@ -32,15 +85,32 @@ class MyApp(QtGui.QMainWindow, Ui_MainWindow):
         tmp.append(out_decoded)
         self.terminal.setText(str(tmp))
         """
-        backup = subprocess.Popen("scrcpy", shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+
+        # process dimension
+        if dimension is None:
+            options = " "
+            pass
+        elif dimension is not None:
+            options = " -m " + str(dimension)
+        else:
+            options = " "
+        backup = subprocess.Popen("scrcpy" + str(options),
+                                  shell=True,
+                                  stdout=subprocess.PIPE,
+                                  stderr=subprocess.STDOUT)
         full = []
+
+        # this block is for instantaneous reading the output
         for line in iter(backup.stdout.readline, b''):
             line = line.rstrip().decode('utf8')
             print(">>>", line)
             full.append(line)
 
         output = '\n'.join(full)
-        self.terminal.setText(str(output))
+        # block ends out
+
+        self.terminal.setText(str(output))  # set text to terminal
+
 
 if __name__ == "__main__":
     app = QtGui.QApplication(sys.argv)

623013096691738bd6f5b67126ad65b2d9b0a4dc
srevinsaju <srevin03@gmail.com>
2019-08-25 08:04:20 +0300

    modified:   .idea/workspace.xml 	modified:   __main__.py 	modified:   bottompanelUI.py 	modified:   bottompanelui.ui 	new file:   guiscrcpy-linux.spec 	deleted:    guiscrcpy.spec 	modified:   mainui.py 	modified:   mainwindow.ui 	modified:   rsrc.qrc 	modified:   toolUI.py 	modified:   toolkit_ui.ui 	icons/ 	rsrc/guiscrcpy-branding.svg 	rsrc_rc.py 	ui/g1595-3.png 	ui/guiscrcpy-branding.png 	ui/guiscrcpy_branding.png 	ui/guiscrcpy_logo.png 	ui/guiscrcpy_logo.svg 	ui/header.png 	ui/rect.png

--- __main__.py
+++ __main__.py
@@ -58,8 +58,25 @@ ln2:: dimensionCheckBox
 ln3:: dimension value
 ln4:: fullScreen
 ln5:: showTouches
+
+
+Icon made by Dave Gandy from www.flaticon.com used under Creative Commons 3.0 Unported. 
+The original SVG black work by Dave Gandy has ben re-oriented, flipped or color-changed. 
+The rest of Terms and Conditions put formward by CC-3.0:Unported has been feverently followed 
+by the debeloper. Icons have been adapeted in all the three windows.
+
+Icons pack obtained from www.flaticon.com
+All rights reserved.
+
 """
+
+
 # removed multiprocess modules
+from PyQt5.QtGui import QPixmap
+from PyQt5 import QtWidgets
+from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtCore import *
+
 import os
 from subprocess import Popen as po, STDOUT
 from subprocess import PIPE
@@ -67,19 +84,14 @@ import sys
 import time
 from PyQt5.QtWidgets import QMessageBox
 from toolUI import Ui_Dialog
+from PyQt5.QtWidgets import QDesktopWidget
 from bottompanelUI import Ui_Panel
 try:
     import psutil
 except ModuleNotFoundError:
     print("psutil is not installed in the python3 directory. "
           "Install with \n $ pip3 install psutil")
-try:
-    from PyQt5 import QtCore, QtGui, QtWidgets
-    from PyQt5.QtCore import *
-except ModuleNotFoundError:
-    print("PySide 2 is not installed. Please install it with pip install PyQt5."
-          "Read the README.md on github.com/srevinsaju/guiscrcpy \n You might want to continue without "
-          "pyAutoGUI limited functionality")
+
 """
 try:
     import pyautogui as auto
@@ -94,16 +106,17 @@ except NotImplementedError:
 """
 from mainui import Ui_MainWindow
 
-build = "1.9.6"
+build = "1.9.7"
+print("guiscrcpy v1.10.0-release")
+print("by srevinsaju")
+print("************************************")
+print("released on 24082019 GMT+0300 2048 ")
+print("************************************")
 
-"""
-Ui_MainWindow, QtBaseClass = uic.loadUiType(qtCreatorFile)
-Ui_Dialog = uic.loadUiType(qtCreatorFile)
-"""
 try:
-    cfg = open(os.path.expanduser("~/guiscrcpy.cfg"), "r")
-    fileExist = True
-    print("User configuration file found!")
+	cfg = open(os.path.expanduser("~/guiscrcpy.cfg"), "r")
+	fileExist = True
+	print("User configuration file found!")
 except FileNotFoundError or FileExistsError:
     print("User configuration file not found!")
     cfg = open(os.path.expanduser("~/guiscrcpy.cfg"), "w+")
@@ -112,40 +125,59 @@ except FileNotFoundError or FileExistsError:
     cfg.close()
 
 if not fileExist:
-    cfg = open(os.path.expanduser("~/guiscrcpy.cfg"), "r+")
-    cfg.writelines(("#" * 25 + "\n", "Created by Srevin Saju\n", "#" * 25 + "\n", str(time.time()) + "\n"))
+	cfg = open(os.path.expanduser("~/guiscrcpy.cfg"), "r+")
+	cfg.writelines(("#" * 25 + "\n", "Created by Srevin Saju\n", "#" * 25 + "\n", str(time.time()) + "\n"))
 
-    cfg.close()
+	cfg.close()
 elif fileExist:
-    cfg = open(os.path.expanduser("~/guiscrcpy.cfg"), "r")
-    """
+	cfg = open(os.path.expanduser("~/guiscrcpy.cfg"), "r")
+	"""
         the cfg file struct::
         :bitrate0 [4]
         :dimension0 [5]
         :swtouches0 [6]
         :fullscreen0 [7]
+        :dispRO0 [8]
         
         
         
-    """
-    try:
-        bitrate0 = cfg.readlines()[4].strip("\n")
-    except IndexError:
-        bitrate0 = 8000
-    try:
-        dimension0 = cfg.readlines()[5].strip("\n")
-    except IndexError:
-        dimension0 = None
-    try:
-        fullscreen0 = cfg.readlines()[7].strip("\n")
-    except IndexError:
-        fullscreen0 = False
-    try:
-        swtouches0 = cfg.readlines()[6].strip("\n")
-    except IndexError:
-        swtouches0 = False
-    print("Bitrate : ", bitrate0, " + Dimensions", dimension0, "")
-    print(bitrate0)
+	"""
+	a = cfg.readlines()
+	cfg.close()
+	# print("cfg:", cfg.readlines())
+	print("cfg:", a)
+	
+	try:
+		bitrate0 = a[4].strip("\n")
+	except IndexError:
+		bitrate0 = 8000
+	try:
+		dimension0 = a[5].strip("\n")
+	except IndexError:
+		dimension0 = None
+	try:
+		fullscreen0 = a[7].strip("\n")
+	except IndexError:
+		fullscreen0 = "False"
+	try:
+	    swtouches0 = a[6].strip("\n")
+	except IndexError:
+	    swtouches0 = "False"
+	
+	try:
+		dispRO0= a[8].strip("\n")
+		print("SUCCESS dispRO")
+	except IndexError:
+		dispRO0 = "False"
+		print("FAILED dispRO")
+	print("Bitrate : ", bitrate0, " + Dimensions", dimension0, "")
+	print(bitrate0)
+	print("dispRO:", dispRO0)
+
+
+
+# ===================
+
 
 
 # BEGIN TOOLKIT.UI
@@ -267,6 +299,8 @@ class MyAppv(Ui_Dialog):
     def __init__(self, Dialog):
         super(MyAppv, self).__init__()
         Ui_Dialog.__init__(self)
+        Dialog.setFixedSize(30, 460)
+        Dialog.setWindowFlags(QtCore.Qt.WindowStaysOnTopHint | QtCore.Qt.FramelessWindowHint)
         self.setupUi(Dialog)
         self.clipD2PC.clicked.connect(clipd2pc)
         self.clipPC2D.clicked.connect(clippc2d)
@@ -283,15 +317,31 @@ class MyAppv(Ui_Dialog):
         self.vdown.clicked.connect(volDN)
         self.potraitUI.clicked.connect(reorientP)
         self.landscapeUI.clicked.connect(reorientL)
+        
+    
 
     def quitn(self):
         print("Quitting")
         sys.exit()
 
+    def mousePressEvent(self, event):
+        self.offset = event.pos()
+
+    def mouseMoveEvent(self, event):
+        x=event.globalX()
+        y=event.globalY()
+        x_w = self.offset.x()
+        y_w = self.offset.y()
+        self.move(x-x_w, y-y_w)
+
 class Panel(Ui_Panel):
     def __init__(self, Dialog):
         super(Panel, self).__init__()
+
         Ui_Panel.__init__(self)
+        # Dialog.setWindowFlags(QtCore.Qt.FramelessWindowHint)
+        Dialog.setWindowFlags(QtCore.Qt.WindowStaysOnTopHint | QtCore.Qt.FramelessWindowHint)
+
         self.setupUi(Dialog)
         self.backk.clicked.connect(Back)
         self.menuUII.clicked.connect(menu)
@@ -299,7 +349,17 @@ class Panel(Ui_Panel):
         self.powerUII.clicked.connect(power)
         self.vupp.clicked.connect(volUP)
         self.vdownn.clicked.connect(volDN)
+        
 
+    def mousePressEvent(self, event):
+        self.offset = event.pos()
+
+    def mouseMoveEvent(self, event):
+        x=event.globalX()
+        y=event.globalY()
+        x_w = self.offset.x()
+        y_w = self.offset.y()
+        self.move(x-x_w, y-y_w)
 
 # END TOOLKIT
 
@@ -314,18 +374,18 @@ def update_terminal():
     tmpRead = tmpTerminal.read()
     return tmpRead
 
-
+"""
 class StartScrcpy(QThread):
     print("Hello")
 
     def __init__(self, options):
         QThread.__init__(self)
         print("SCRCPY launch")
-        backup = po("scrcpy" + str(options),
-                                  shell=True,
-                                  stdin=PIPE,
-                                  stdout=PIPE,
-                                  stderr=STDOUT)
+        # backup = po("scrcpy" + str(options),
+        #                          shell=True,
+        #                          stdin=PIPE,
+        #                          stdout=PIPE,
+        #                          stderr=STDOUT)
 
     def __del__(self):
         self.wait()
@@ -343,7 +403,7 @@ class StartScrcpy(QThread):
 
     def run(self):
         pass
-
+"""
 
 def readThreadStdOut():
     someFile = open("user.history", "w+")
@@ -397,19 +457,37 @@ class MyApp(Ui_MainWindow):
         # self.menuAbout.itemPressed.connect(self.menu_about)
 
         # check if process Scrcpy is running right now in while loop
+        """
+        background-color: qlineargradient(spread:pad, x1:0, y1:0, x2:1, y2:1, stop:0 rgba(0, 255, 255, 255), stop:1 rgba(0, 255, 152, 255));
+color: rgb(0, 0, 0);
+border-radius: 10px;
+border-color: qlineargradient(spread:pad, x1:0, y1:0, x2:1, y2:0, stop:0 rgba(0, 0, 255, 255), stop:1 rgba(255, 0, 255, 255));
+        """
+        
+
+
         bit_rate = bitrate0
         dimensions = dimension0
         swtouches = swtouches0
+        dispRO = dispRO0
         fullscreen_opt = fullscreen0
+        print("OPTS:", bit_rate, dimensions, swtouches, dispRO, fullscreen_opt)
         self.dial.setValue(int(bit_rate))
-        if (swtouches):
+        if (swtouches.find("True")>-1):
             self.showTouches.setChecked(True)
         else:
             self.showTouches.setChecked(False)
+        if (dispRO.find("True")>-1):
+            self.displayForceOn.setChecked(True)
+        else:
+            self.displayForceOn.setChecked(False)
         if dimensions != None:
             self.dimensionDefaultCheckbox.setChecked(False)
-            self.dimensionSlider.setValue(dimensions)
-        if (fullscreen_opt):
+            try:
+            	self.dimensionSlider.setValue(dimensions)
+            except TypeError:
+            	self.dimensionDefaultCheckbox.setChecked(True)
+        if (fullscreen_opt.find("True")>-1):
             self.fullscreen.setChecked(True)
         else:
             self.fullscreen.setChecked(False)
@@ -431,13 +509,20 @@ class MyApp(Ui_MainWindow):
 
         # MAIN EXECUTE ACTION
         self.executeaction.clicked.connect(self.start_act)
+
         self.quit.clicked.connect(self.quitAct)
         self.dimensionText.setText("DEFAULT")
         bit_rate = int(self.dial.value())
-        self.bitrateText.setText(str(bit_rate) + "KB/s")
+        self.bitrateText.setText(" " + str(bit_rate) + "KB/s")
         self.pushButton.setText("RESET")
         self.pushButton.clicked.connect(self.reset)
+        self.abtme.clicked.connect(self.openme)
+        self.abtgit.clicked.connect(self.opengit)
 
+    def openme(self):
+        webbrowser.open("https://srevinsaju.wixsite.com/srevinsaju")
+    def opengit(self):
+        webbrowser.open("https://github.com/srevinsaju/guiscrcpy")
     def about(self):
         abtBox = QMessageBox().window()
         abtBox.about(self.pushButton, "Info", "Please restart GUIscrcpy to reset the settings. GUIscrcpy will now exit")
@@ -472,7 +557,7 @@ class MyApp(Ui_MainWindow):
             self.dimensionSlider.setEnabled(True)
             dimension = int(self.dimensionSlider.value())
             dimension0 = int(self.dimensionSlider.value())
-            self.dimensionText.setText(str(dimension) + "px")
+            self.dimensionText.setText(" "+str(dimension) + "px")
             self.dimensionSlider.sliderMoved.connect(self.slider_text_refresh)
             self.dimensionSlider.sliderReleased.connect(self.slider_text_refresh)
 
@@ -490,7 +575,10 @@ class MyApp(Ui_MainWindow):
         pass
 
     def start_act(self):
+    	
+
         self.runningNot.setText("CHECKING DEVICE CONNECTION")
+        timei = time.time()
         self.progressBar.setValue(5)
         adb_chk = po("adb devices", shell=True, stdout=PIPE)
         output = adb_chk.stdout.readlines()
@@ -565,6 +653,9 @@ class MyApp(Ui_MainWindow):
             self.options += " --always-on-top"
         if self.fullscreen.isChecked():
             self.options += " -f"
+            fullscreen0 = True
+        else:
+        	fullscreen0 = False
         """
         if self.keepdisplayRO.isChecked():
             self.options += " --no-control"
@@ -573,12 +664,29 @@ class MyApp(Ui_MainWindow):
         if self.showTouches.isChecked():
             self.options += " --show-touches"
             swtouches0 = True
+
             """        if self.keepdisplayRO.isChecked():
             self.options += " --turn-screen-off"
             """
 
         else:
             swtouches0 = False
+        if self.recScui.isChecked():
+            self.options += " -r "+str(int(time.time()))+".mp4 "
+            
+            """        if self.keepdisplayRO.isChecked():
+            self.options += " --turn-screen-off"
+            """
+        if self.displayForceOn.isChecked():
+            self.options += " -S"
+            dispRO0 = True
+            """        if self.keepdisplayRO.isChecked():
+            self.options += " --turn-screen-off"
+            """
+
+        else:
+            dispRO0 = False
+
 
         self.options += " -b " + str(int(self.dial.value())) + "K"
         bitrate0 = str(int(self.dial.value()))
@@ -610,14 +718,27 @@ class MyApp(Ui_MainWindow):
         """
         print("FLAGS PASSED : " + self.options)
         self.progressBar.setValue(75)
-        StartScrcpy(options=self.options)
+        backup = po("bash -c 'scrcpy" + str(self.options)+"'",
+                                  shell=True,
+                                  stdin=PIPE,
+                                  stdout=PIPE,
+                                  stderr=STDOUT)
+        # StartScrcpy(options=self.options)
+        
+        timef = time.time()
+        eta = timef-timei
+        print("SCRCPY is launched in", eta, "seconds")
         self.progressBar.setValue(100)
 
         # self.terminal.setText(full)
         cfg = open(os.path.expanduser("~/guiscrcpy.cfg"), "w+")
+        print("writing: #" * 25 + "\n", "Created by Srevin Saju\n", "#" * 25 + "\n", str(time.time()) + "\n",
+                        str(bitrate0) + "\n", str(dimension0) + "\n", str(swtouches0) + "\n",
+                        str(fullscreen0) + "\n" + str(dispRO0) + "\n")
         cfg.writelines(("#" * 25 + "\n", "Created by Srevin Saju\n", "#" * 25 + "\n", str(time.time()) + "\n",
                         str(bitrate0) + "\n", str(dimension0) + "\n", str(swtouches0) + "\n",
-                        str(fullscreen0) + "\n"))
+                        str(fullscreen0) + "\n" + str(dispRO0) + "\n"))
+
         cfg.close()
         """
         p5 = multiprocessing.Process(target=update_terminal)
@@ -627,19 +748,38 @@ class MyApp(Ui_MainWindow):
 
 
 if __name__ == "__main__":
-    app = QtWidgets.QApplication(sys.argv)
-    # appo = QtWidgets.QApplication(sys.argv)
-    # app.aboutToQuit().connect(app.deleteLater)
-    window = QtWidgets.QMainWindow()
-    windoww = QtWidgets.QMainWindow()
-    windowww = QtWidgets.QMainWindow()
-    prog = MyApp(window)
-    panel = Panel(windoww)
-    progg = MyAppv(windowww)
-    window.show()
-    windowww.show()
-    windoww.show()
-    app.exec_()
-    # appo.exec_()
+	
+	app = QtWidgets.QApplication(sys.argv)
+	splash_pix = QPixmap(':/res/ui/guiscrcpy-branding.png')
+	splash = QtWidgets.QSplashScreen(splash_pix)
+	splash.setMask(splash_pix.mask())
+	splash.show()
+	app.processEvents()
+	adb_chk0 = po("adb devices -l", shell=True, stdout=PIPE, stderr=PIPE)
+	time.sleep(1)
+	app.processEvents()
+	print("output:", adb_chk0.stdout)
+	"""
+	Ui_MainWindow, QtBaseClass = uic.loadUiType(qtCreatorFile)
+	Ui_Dialog = uic.loadUiType(qtCreatorFile)
+	"""
 
-    sys.exit()
+
+	window = QtWidgets.QMainWindow()
+	windoww = QtWidgets.QMainWindow()
+    
+
+	windowww = QtWidgets.QMainWindow()
+	prog = MyApp(window)
+	panel = Panel(windoww)
+	progg = MyAppv(windowww)
+
+	window.show()
+	splash.hide()
+	windowww.show()
+	windoww.show()
+	
+	app.exec_()
+	# appo.exec_()
+
+	sys.exit()
--- bottompanelUI.py
+++ bottompanelUI.py
@@ -12,75 +12,116 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 class Ui_Panel(object):
     def setupUi(self, Panel):
         Panel.setObjectName("Panel")
-        Panel.resize(312, 26)
+        Panel.resize(328, 26)
         icon = QtGui.QIcon()
-        icon.addPixmap(QtGui.QPixmap("rsrc/android_circle.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        icon.addPixmap(QtGui.QPixmap(":/res/ui/guiscrcpy_logo.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
         Panel.setWindowIcon(icon)
+        Panel.setStyleSheet("\n"
+".QPushButton {\n"
+"border-radius: 1px;\n"
+"color: rgb(0, 0, 0);\n"
+" \n"
+"    background-color: qlineargradient(spread:pad, x1:0, y1:0.915182, x2:0, y2:0.926, stop:0.897059 rgba(41, 41, 41, 255), stop:1 rgba(30, 30, 30, 255));\n"
+"                    }\n"
+"\n"
+"QPushButton:pressed {\n"
+"border-radius: 5px;\n"
+"                      \n"
+"background-color: qlineargradient(spread:pad, x1:0, y1:0, x2:1, y2:1, stop:0 rgba(0, 255, 255, 255), stop:1 rgba(0, 255, 152, 255));\n"
+"color: rgb(0, 0, 0);\n"
+"                        }\n"
+"QPushButton:hover {\n"
+"border-radius: 5px;\n"
+"                      \n"
+"    background-color: qlineargradient(spread:pad, x1:0, y1:0, x2:1, y2:1, stop:0 rgba(0, 199, 199, 255), stop:1 rgba(0, 190, 113, 255));\n"
+"color: rgb(0, 0, 0);\n"
+"                        }\n"
+"")
         self.backk = QtWidgets.QPushButton(Panel)
         self.backk.setEnabled(True)
-        self.backk.setGeometry(QtCore.QRect(190, 0, 81, 25))
-        self.backk.setStyleSheet("background-color:rgb(171, 171, 171)")
+        self.backk.setGeometry(QtCore.QRect(210, 0, 51, 25))
+        self.backk.setStyleSheet("")
         self.backk.setText("")
         icon1 = QtGui.QIcon()
-        icon1.addPixmap(QtGui.QPixmap("rsrc/left-arrow-1.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        icon1.addPixmap(QtGui.QPixmap(":/icons/icons/chevron-sign-left.svg"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
         self.backk.setIcon(icon1)
         self.backk.setObjectName("backk")
         self.powerUII = QtWidgets.QPushButton(Panel)
         self.powerUII.setEnabled(True)
-        self.powerUII.setGeometry(QtCore.QRect(40, 0, 31, 25))
-        self.powerUII.setStyleSheet("background-color: rgb(189, 189, 189);")
+        self.powerUII.setGeometry(QtCore.QRect(20, 0, 61, 25))
+        self.powerUII.setStyleSheet("")
         self.powerUII.setText("")
         icon2 = QtGui.QIcon()
-        icon2.addPixmap(QtGui.QPixmap("rsrc/power.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        icon2.addPixmap(QtGui.QPixmap(":/icons/icons/power.svg"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
         self.powerUII.setIcon(icon2)
         self.powerUII.setCheckable(False)
         self.powerUII.setObjectName("powerUII")
         self.menuUII = QtWidgets.QPushButton(Panel)
         self.menuUII.setEnabled(True)
-        self.menuUII.setGeometry(QtCore.QRect(70, 0, 51, 25))
-        self.menuUII.setStyleSheet("background-color:rgb(171, 171, 171)")
+        self.menuUII.setGeometry(QtCore.QRect(90, 0, 51, 25))
+        self.menuUII.setStyleSheet("")
         self.menuUII.setText("")
         icon3 = QtGui.QIcon()
-        icon3.addPixmap(QtGui.QPixmap("rsrc/menu.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        icon3.addPixmap(QtGui.QPixmap(":/icons/icons/reorder-option.svg"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
         self.menuUII.setIcon(icon3)
         self.menuUII.setObjectName("menuUII")
-        self.vupp = QtWidgets.QPushButton(Panel)
-        self.vupp.setEnabled(True)
-        self.vupp.setGeometry(QtCore.QRect(0, 0, 31, 25))
-        self.vupp.setStyleSheet("background-color:rgb(171, 171, 171)")
-        self.vupp.setText("")
-        icon4 = QtGui.QIcon()
-        icon4.addPixmap(QtGui.QPixmap("rsrc/volume.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
-        self.vupp.setIcon(icon4)
-        self.vupp.setObjectName("vupp")
-        self.homee = QtWidgets.QPushButton(Panel)
-        self.homee.setEnabled(True)
-        self.homee.setGeometry(QtCore.QRect(120, 0, 71, 25))
-        self.homee.setStyleSheet("background-color:rgb(171, 171, 171);\n"
-"border-color: rgb(0, 0, 0);")
-        self.homee.setText("")
-        icon5 = QtGui.QIcon()
-        icon5.addPixmap(QtGui.QPixmap("rsrc/home.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
-        self.homee.setIcon(icon5)
-        self.homee.setObjectName("homee")
         self.vdownn = QtWidgets.QPushButton(Panel)
         self.vdownn.setEnabled(True)
-        self.vdownn.setGeometry(QtCore.QRect(280, 0, 31, 25))
-        self.vdownn.setStyleSheet("background-color:rgb(171, 171, 171)")
+        self.vdownn.setGeometry(QtCore.QRect(270, 0, 31, 25))
+        self.vdownn.setStyleSheet("")
         self.vdownn.setText("")
+        icon4 = QtGui.QIcon()
+        icon4.addPixmap(QtGui.QPixmap(":/icons/icons/reduced-volume.svg"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
         self.vdownn.setIcon(icon4)
         self.vdownn.setObjectName("vdownn")
+        self.homee = QtWidgets.QPushButton(Panel)
+        self.homee.setEnabled(True)
+        self.homee.setGeometry(QtCore.QRect(140, 0, 71, 25))
+        self.homee.setStyleSheet("")
+        self.homee.setText("")
+        icon5 = QtGui.QIcon()
+        icon5.addPixmap(QtGui.QPixmap(":/icons/icons/home.svg"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        self.homee.setIcon(icon5)
+        self.homee.setObjectName("homee")
+        self.vupp = QtWidgets.QPushButton(Panel)
+        self.vupp.setEnabled(True)
+        self.vupp.setGeometry(QtCore.QRect(300, 0, 31, 25))
+        self.vupp.setStyleSheet("")
+        self.vupp.setText("")
+        icon6 = QtGui.QIcon()
+        icon6.addPixmap(QtGui.QPixmap(":/icons/icons/volume-up-interface-symbol.svg"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        self.vupp.setIcon(icon6)
+        self.vupp.setObjectName("vupp")
+        self.label = QtWidgets.QLabel(Panel)
+        self.label.setGeometry(QtCore.QRect(0, -10, 20, 41))
+        font = QtGui.QFont()
+        font.setPointSize(18)
+        font.setBold(True)
+        font.setWeight(75)
+        self.label.setFont(font)
+        self.label.setScaledContents(True)
+        self.label.setAlignment(QtCore.Qt.AlignCenter)
+        self.label.setObjectName("label")
+        self.homee.raise_()
+        self.backk.raise_()
+        self.powerUII.raise_()
+        self.menuUII.raise_()
+        self.vdownn.raise_()
+        self.vupp.raise_()
+        self.label.raise_()
 
         self.retranslateUi(Panel)
         QtCore.QMetaObject.connectSlotsByName(Panel)
 
     def retranslateUi(self, Panel):
         _translate = QtCore.QCoreApplication.translate
-        Panel.setWindowTitle(_translate("Panel", "Main Panel"))
+        Panel.setWindowTitle(_translate("Panel", "guiscrcpy"))
         self.backk.setToolTip(_translate("Panel", "Back key"))
         self.powerUII.setToolTip(_translate("Panel", "Power on/off"))
         self.menuUII.setToolTip(_translate("Panel", "Menu key"))
-        self.vupp.setToolTip(_translate("Panel", "Volume Up"))
+        self.vdownn.setToolTip(_translate("Panel", "Volume Up"))
         self.homee.setToolTip(_translate("Panel", "Home key"))
+        self.label.setText(_translate("Panel", "::"))
 
 
+import rsrc_rc

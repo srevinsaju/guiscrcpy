52d01b4721ee341ebbc4890707279953698b94d8
Srevin Saju <srevin03@gmail.com>
2019-09-11 10:05:14 +0300

    new file:   Backup/swipeUX.py 	new file:   Backup/untitled.ui 	modified:   __main__.py 	new file:   icons/chevron-sign-down.svg 	new file:   icons/chevron-sign-right.svg 	new file:   icons/chevron-sign-up.svg 	modified:   rsrc.qrc 	modified:   rsrc_rc.py 	new file:   srevinsaju

--- __main__.py
+++ __main__.py
@@ -687,7 +687,173 @@ class MyAppv(QMainWindow):
         sys.exit()
 
 
+class SwipeUX(QMainWindow):
+    def __init__(self):
+        super(SwipeUX, self).__init__()
+        self.oldPos = None
+        self.setObjectName("SwipeUX")
+        self.setWindowFlags(Qt.FramelessWindowHint)
+        self.setAttribute(Qt.WA_NoSystemBackground, True)
+        self.setAttribute(Qt.WA_TranslucentBackground, True)
+        
+        # self.setWindowFlags(QtCore.Qt.WindowStaysOnTopHint | QtCore.Qt.FramelessWindowHint)
+        self.resize(70, 70)
+        # -----------------------
+        # =====================
+        icon = QtGui.QIcon()
+        icon.addPixmap(QtGui.QPixmap(":/res/ui/guiscrcpy_logo.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        self.setWindowIcon(icon)
+        self.setStyleSheet("QWidget{background-color: rgba(0,0,0,30);}\nQPushButton {\n"
+"border-radius: 15px;\n"
+"    background-color: qradialgradient(spread:pad, cx:0.5, cy:0.5, radius:0.5, fx:0.495098, fy:0.5, stop:0.887255 rgba(35, 35, 35, 255), stop:0.901961 rgba(0, 0, 0, 255));\n"
+"color: rgb(0, 0, 0);\n"
+"\n"
+"}\\n\n"
+"QPushButton:pressed {\n"
+"border-radius: 15px;\n"
+"\n"
+"background-color: qlineargradient(spread:pad, x1:0, y1:0, x2:1, y2:1, stop:0 rgba(0, 255, 255, 255), stop:1 rgba(0, 255, 152, 255));\n"
+"color: rgb(0, 0, 0);\n"
+"   }\n"
+"QMainWindow{background-color: rgba(0,0,0,30);}\n"
+"QPushButton:hover {\n"
+"border-radius: 15px;\n"
+"background-color: qlineargradient(spread:pad, x1:0, y1:0, x2:1, y2:1, stop:0 rgba(0, 199, 199, 255), stop:1 rgba(0, 190, 113, 255));\n"
+"color: rgb(0, 0, 0);\n"
+"}")
+        self.centralwidget = QtWidgets.QWidget(self)
+        self.centralwidget.setObjectName("centralwidget")
+        self.swirt = QtWidgets.QPushButton(self.centralwidget)
+        self.swirt.setGeometry(QtCore.QRect(40, 20, 30, 30))
+        self.swirt.setText("")
+        icon1 = QtGui.QIcon()
+        icon1.addPixmap(QtGui.QPixmap(":/icons/icons/chevron-sign-right.svg"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        self.swirt.setIcon(icon1)
+        self.swirt.setObjectName("swirt")
+        self.swilf = QtWidgets.QPushButton(self.centralwidget)
+        self.swilf.setGeometry(QtCore.QRect(0, 20, 30, 30))
+        self.swilf.setText("")
+        icon2 = QtGui.QIcon()
+        icon2.addPixmap(QtGui.QPixmap(":/icons/icons/chevron-sign-left.svg"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        self.swilf.setIcon(icon2)
+        self.swilf.setObjectName("swilf")
+        self.swidn = QtWidgets.QPushButton(self.centralwidget)
+        self.swidn.setGeometry(QtCore.QRect(20, 40, 30, 30))
+        self.swidn.setText("")
+        icon3 = QtGui.QIcon()
+        icon3.addPixmap(QtGui.QPixmap(":/icons/icons/chevron-sign-down.svg"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        self.swidn.setIcon(icon3)
+        self.swidn.setObjectName("swidn")
+        self.swiup = QtWidgets.QPushButton(self.centralwidget)
+        self.swiup.setGeometry(QtCore.QRect(20, 0, 30, 30))
+        self.swiup.setText("")
+        icon4 = QtGui.QIcon()
+        icon4.addPixmap(QtGui.QPixmap(":/icons/icons/chevron-sign-up.svg"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        self.swiup.setIcon(icon4)
+        self.swiup.setObjectName("swiup")
+        self.setCentralWidget(self.centralwidget)
+        # -----------------
+        # ================
+        
+        self.oldpos = self.pos()
+        
+        self.swiup.pressed.connect(self.swipup)
+        self.swidn.pressed.connect(self.swipdn)
+        self.swilf.pressed.connect(self.swipleft)
+        self.swirt.pressed.connect(self.swipright)
+    
+    def paintEvent(self, event):
+        qp = QtGui.QPainter()
+        qp.begin(self)
+        qp.setRenderHint(QtGui.QPainter.Antialiasing);
+        qp.setPen(Qt.NoPen);
+        qp.setBrush(QtGui.QColor(0, 0, 0, 127));
+        qp.drawEllipse(0, 0, 70, 70);
+        qp.end()
+    def mousePressEvent(self, event):
+        self.oldPos = event.globalPos()
+        # print("HIT")
 
+    def mouseMoveEvent(self, event):
+        delta = QPoint (event.globalPos() - self.oldPos)
+
+        self.move(self.x() + delta.x(), self.y() + delta.y())
+        self.oldPos = event.globalPos()
+    
+    def swipdn(self):
+        print("LOG: Passing SWIPE DOWN")
+        adb_dim = po("adb shell wm size", shell=True, stdout=PIPE, stderr=PIPE)
+        out = adb_dim.stdout.read()
+        out_decoded = out.decode("utf-8")
+        out_decoded = out_decoded[:-1]
+        dimVal = out_decoded.split(": ")
+        dimensions_ = dimVal[1]
+        dimValues = dimensions_.split("x")
+        posy = int(dimValues[1])
+        posx = int(dimValues[0])
+        
+        newposx = posx/2 # find center
+        
+        adb_pull = po("adb shell input swipe "+ str(newposx) + " 200 " + str(newposx) + " " +str(posy-200) ,
+                                    shell=True, stdout=PIPE,
+                                    stderr=PIPE)
+
+    def swipup(self):
+        print("LOG: Passing SWIPE UP")
+        adb_dim = po("adb shell wm size", shell=True, stdout=PIPE, stderr=PIPE)
+        out = adb_dim.stdout.read()
+        out_decoded = out.decode("utf-8")
+        out_decoded = out_decoded[:-1]
+        dimVal = out_decoded.split(": ")
+        dimensions__ = dimVal[1]
+        dimValues = dimensions__.split("x")
+        posy = int(dimValues[1])
+        posx = int(dimValues[0])
+        
+        newposx = posx/2 # find center
+        
+        adb_pull = po("adb shell input swipe " + str(newposx) + " "  + str(posy-200) + " " + str(newposx) + " 200",
+                                    shell=True, stdout=PIPE,
+                                    stderr=PIPE)
+    
+    def swipleft(self):
+        print("LOG: Passing SWIPE LEFT")
+        adb_dim = po("adb shell wm size", shell=True, stdout=PIPE, stderr=PIPE)
+        out = adb_dim.stdout.read()
+        out_decoded = out.decode("utf-8")
+        out_decoded = out_decoded[:-1]
+        dimVal = out_decoded.split(": ")
+        dimensions__ = dimVal[1]
+        dimValues = dimensions__.split("x")
+        posy = int(dimValues[1])
+        posx = int(dimValues[0])
+        
+        newposy = posy/2 # find center
+        
+        adb_pull = po("adb shell input swipe " + str(10) + " "  + str(newposy) + " " + str(posx-10) + " "+str(newposy),
+                                    shell=True, stdout=PIPE,
+                                    stderr=PIPE)
+    
+    def swipright(self):
+        print("LOG: Passing SWIPE RIGHT")
+        adb_dim = po("adb shell wm size", shell=True, stdout=PIPE, stderr=PIPE)
+        out = adb_dim.stdout.read()
+        out_decoded = out.decode("utf-8")
+        out_decoded = out_decoded[:-1]
+        dimVal = out_decoded.split(": ")
+        dimensions_ = dimVal[1]
+        dimValues = dimensions_.split("x")
+        posy = int(dimValues[1])
+        posx = int(dimValues[0])
+        
+        newposy = posy/2 # find center
+        
+        adb_pull = po("adb shell input swipe " + str(posx-10) + " "  + str(newposy) + " " + str(10) + " "+str(newposy),
+                                    shell=True, stdout=PIPE,
+                                    stderr=PIPE)
+
+
+        
 class Panel(QMainWindow):
     # there was a Dialog in the bracket
     def __init__(self):
@@ -1370,7 +1536,10 @@ if __name__ == "__main__":
 	Ui_MainWindow, QtBaseClass = uic.loadUiType(qtCreatorFile)
 	Ui_Dialog = uic.loadUiType(qtCreatorFile)
 	"""
-
+        
+        rw = SwipeUX()
+        rw.show()
+        
         window = QtWidgets.QMainWindow()
         app.setStyleSheet(qdarkstyle.load_stylesheet_pyqt5())
         # windoww = QtWidgets.QMainWindow()

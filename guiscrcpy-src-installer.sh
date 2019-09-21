dc4e0bcc1864dd8be9d3787e876743bb235975fc
Srevin Saju <srevin03@gmail.com>
2019-09-21 22:32:02 +0300

    modified:   guiscrcpy-src-installer.sh

--- guiscrcpy-src-installer.sh
+++ guiscrcpy-src-installer.sh
@@ -2,20 +2,26 @@
 echo guiscrcpy-installer by srevinsaju
 echo All Rights Reserved
 echo ====================
-rm -r ~/.local/bin/guiscrc*
-rm -r ~/.local/share/guiscr*
+echo Installing dependencies
+echo Installing scrcpy
+sudo snap install scrcpy
+echo Installing python modules
+cat requirements.txt
+python3 -m pip install -r requirements.txt
+sudo rm -r ~/.local/bin/guiscrc*
+sudo rm -r ~/.local/share/guiscr*
 echo Removing obsolete files
-mkdir ~/.local/share/guiscrcpy
-cp -r guiscrcpy ~/.local/share/guiscrcpy
+sudo mkdir ~/.local/share/guiscrcpy
+sudo cp -r guiscrcpy ~/.local/share/guiscrcpy
 echo Copying scripts ... 
-cp -r installers/linux/guiscrcpy-src-launcher.sh ~/.local/bin/guiscrcpy
-chmod +x ~/.local/bin/guiscrcpy
+sudo cp -r installers/linux/guiscrcpy-src-launcher.sh ~/.local/bin/guiscrcpy
+sudo chmod +x ~/.local/bin/guiscrcpy
 echo Adding Desktop Launcher
-rm -r ~/.local/share/applications/guiscrcpy.desktop
-rm -r /usr/share/guiscrcpy
-mkdir /usr/share/guiscrcpy
+sudo rm -r ~/.local/share/applications/guiscrcpy.desktop
+sudo rm -r /usr/share/guiscrcpy
+sudo mkdir /usr/share/guiscrcpy
 
-cp installers/linux/icons/guiscrcpy_logo.png /usr/share/guiscrcpy/
-cp installers/linux/icons/guiscrcpy.desktop ~/.local/share/applications/
+sudo cp installers/linux/icons/guiscrcpy_logo.png /usr/share/guiscrcpy/
+sudo cp installers/linux/icons/guiscrcpy.desktop ~/.local/share/applications/
 echo Script Succeeded
-
+echo Now You can run guiscrcpy by running 'guiscrcpy' from terminal or from your application launcher. 

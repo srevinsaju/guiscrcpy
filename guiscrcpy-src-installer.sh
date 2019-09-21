c1ac34c5034d14bbc879c471c8faedd605d73b0e
Srevin Saju <srevin03@gmail.com>
2019-09-21 19:25:40 +0300

    modified:   guiscrcpy-src-installer.sh 	modified:   installers/linux/guiscrcpy-src-launcher.sh 	deleted:    installers/linux/guiscrcpy.desktop 	deleted:    installers/linux/guiscrcpy_logo.png

--- guiscrcpy-src-installer.sh
+++ guiscrcpy-src-installer.sh
@@ -10,3 +10,12 @@ cp -r guiscrcpy ~/.local/share/guiscrcpy
 echo Copying scripts ... 
 cp -r installers/linux/guiscrcpy-src-launcher.sh ~/.local/bin/guiscrcpy
 chmod +x ~/.local/bin/guiscrcpy
+echo Adding Desktop Launcher
+rm -r ~/.local/share/applications/guiscrcpy.desktop
+rm -r /usr/share/guiscrcpy
+mkdir /usr/share/guiscrcpy
+
+cp installers/linux/icons/guiscrcpy_logo.png /usr/share/guiscrcpy/
+cp installers/linux/icons/guiscrcpy.desktop ~/.local/share/applications/
+echo Script Succeeded
+
--- installers/linux/guiscrcpy-src-launcher.sh
+++ installers/linux/guiscrcpy-src-launcher.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-python3 ~/.local/share/guiscrcpy
+python3 ~/.local/share/guiscrcpy/guiscrcpy

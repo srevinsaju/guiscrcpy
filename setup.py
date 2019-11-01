31309d636995b675a7a1404423014f89b87ec053
Srevin Saju <srevin03@gmail.com>
2019-11-01 08:44:55 +0300

    Gitpython ig

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name = 'guiscrcpy',
-    version = '1.11.3'+sha[:7],
+    version = repo.git.describe("--tags"),
     description = 'An Open Source - Fast -  Android Screen Mirroring system.',
     long_description=long_description,
     long_description_content_type='text/markdown',

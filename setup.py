5352637baee63e1031a15c2f5c3990fac9eed077
Srevin Saju <srevin03@gmail.com>
2019-11-01 08:42:25 +0300

    Gitpython ig

--- setup.py
+++ setup.py
@@ -10,6 +10,7 @@ this_directory = path.abspath(path.dirname(__file__))
 with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
+
 setup(
     name = 'guiscrcpy',
     version = '1.11.3'+sha[:7],

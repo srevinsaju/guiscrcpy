0bcebc1881a2bd777ed000d3c254165ae43a4e03
Srevin Saju <srevin03@gmail.com>
2020-01-28 15:35:20 +0300

    Fix __init_- for windows systems

--- guiscrcpy/__init__.py
+++ guiscrcpy/__init__.py
@@ -1,10 +1,1 @@
-import os
-from guiscrcpy import __main__
-import sys
-import re
-import guiscrcpy
-patz = list(guiscrcpy.__path__)[0]
-sys.path.append(patz)
-sys.path.append('')
-# print("PATH: ", sys.path)
-os.chdir(patz)
+__version__ = 2.0

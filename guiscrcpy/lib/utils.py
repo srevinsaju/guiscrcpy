c2e3bbc68a92e19efbceae36f901a911f8abcacf
Srevin Saju <srevin03@gmail.com>
2020-01-30 20:33:24 +0300

    Add missing import shlex. Closes #52

--- guiscrcpy/lib/utils.py
+++ guiscrcpy/lib/utils.py
@@ -17,7 +17,7 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """
 
-
+import shlex
 import logging
 import os
 from guiscrcpy.platform.platform import System

0442d27bdd0da62bd1d51327feba75b3f17c0b7c
Srevin Saju <srevin03@gmail.com>
2020-02-21 19:49:13 +0300

    Fix connect signals

--- guiscrcpy/ux/network.py
+++ guiscrcpy/ux/network.py
@@ -40,7 +40,7 @@ class InterfaceNetwork(QMainWindow, Ui_NetworkUI):
         self.nm_connect.pressed.connect(self.connect)
         self.nm_refresh.pressed.connect(self.refresh)
         self.nm_det.setText("Click Refresh to load IP addresses")
-        self.tcpip.connect.pressed.connect(self.tcpip_launch)
+        self.tcpip.pressed.connect(self.tcpip_launch)
 
     def tcpip_launch(self):
         adb.command(adb.path, 'tcpip')

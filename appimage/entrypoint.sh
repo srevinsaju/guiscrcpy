4f1b5b80c1c6537683075e1eb6a5b4eb566cc358
Srevin Saju <srevin03@gmail.com>
2020-04-20 22:14:46 +0300

    Move sed to appimage/entrypoint.sh

--- appimage/entrypoint.sh
+++ appimage/entrypoint.sh
@@ -1,1 +1,8 @@
+export LD_LIBRARY_PATH="${APPDIR}/usr/lib:${LD_LIBRARY_PATH}"
+export GUISCRCPY_SCRCPY="${APPDIR}/usr/bin/scrcpy"
+export GUISCRCPY_ADB="${APPDIR}/usr/bin/adb"
+export ADB="${APPDIR}/usr/bin/adb"
+export SCRCPY_SERVER_PATH="${APPDIR}/usr/share/scrcpy/scrcpy-server"
+export PATH="${APPDIR}/usr/bin:${PATH}"
+
 {{ python-executable }} -s ${APPDIR}/opt/python{{ python-version }}/bin/guiscrcpy "$@"

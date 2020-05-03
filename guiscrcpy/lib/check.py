895372ec7ae4ca39333e69872e8551229198c03a
Srevin Saju <srevin03@gmail.com>
2020-05-03 22:07:08 +0300

    Add a detailed device (adb devices -l) parser for guiscrcpy.adb

--- guiscrcpy/lib/check.py
+++ guiscrcpy/lib/check.py
@@ -154,3 +154,27 @@ class adb:
 
         logging.debug("ADB: {}".format(output))
         return output
+
+    @staticmethod
+    def devices_detailed(increment=''):
+        if increment is None:
+            raise FileNotFoundError(
+                "guiscrcpy couldn't find adb. "
+                "Please specify path to adb in configuration filename"
+            )
+        proc = Popen(_(increment + " devices -l"), stdout=PIPE)
+        output = [[y.strip() for y in x.split()]
+                  for x in decode_process(proc)[1:]][:-1]
+        devices_found = []
+        for device in output:
+            description = {
+                'identifier': device[0],
+                'status': device[1],
+                'product': device[2].split(':')[1],
+                'model': device[3].split(':')[1],
+                'device': device[4].split(':')[1],
+                'transport_id': device[5].split(':')[1]
+            }
+            devices_found.append(description)
+        logging.debug("ADB: {}".format(devices_found))
+        return devices_found

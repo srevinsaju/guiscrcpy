e42cca4f31884af4c4262c728072c48bcb7a33aa
Srevin Saju <srevin03@gmail.com>
2020-05-03 22:53:39 +0300

    Add method to check for unpaired and new devices

--- guiscrcpy/launcher.py
+++ guiscrcpy/launcher.py
@@ -568,6 +568,85 @@ class InterfaceGuiscrcpy(QMainWindow, Ui_MainWindow):
         else:
             raise ValueError("No item is selected in QListView")
 
+    def scan_devices_update_list_view(self):
+        """
+        Scan for new devices; and update the list view
+        :return:
+        """
+        # self.devices_view.clear()
+        paired_devices = []
+        for index in range(self.devices_view.count()):
+            paired_devices.append(self.devices_view.item(index))
+
+        devices = adb.devices_detailed(adb.path)
+        log(devices)
+        for i in devices:
+
+            if i['identifier'] not in config['device'].keys():
+                device_paired_and_exists = False
+                config['device'][i['identifier']] = {
+                    'rotation': 0
+                }
+            else:
+                device_paired_and_exists = True
+
+            if config['device'].get('rotation', 0) in (-1, 0, 2):
+                icon = ':/icons/icons/portrait_mobile_white.svg'
+            else:
+                icon = ':/icons/icons/landscape_mobile_white.svg'
+
+            if i['status'] == 'offline':
+                icon = ':/icons/icons/portrait_mobile_error.svg'
+            elif i['status'] == 'unauthorized':
+                icon = ':/icons/icons/portrait_mobile_warning.svg'
+
+            # check if device is paired
+            # if yes, just update the list item
+            if not device_paired_and_exists:
+                paired = False
+                devices_view_list_item = QListWidgetItem(QIcon(icon), '')
+            else:
+                for paired_device in paired_devices:
+                    if paired_device.text().split()[0] == i['model']:
+                        paired = True
+                        devices_view_list_item = paired_device
+                        break
+                else:
+                    paired = False
+                    devices_view_list_item = QListWidgetItem(QIcon(icon), '')
+
+            devices_view_list_item.setIcon(QIcon(icon))
+
+            devices_view_list_item.setText(
+                "{device}\n{mode}\n{status}".format(
+                    device=i['model'],
+                    mode=i['identifier'],
+                    status=i['status']
+                )
+            )
+            devices_view_list_item.setToolTip(
+                "Device: {d}\n"
+                "Model: {m}\n"
+                "Alias: {a}\n"
+                "Status: {s}\n"
+                "Transport ID: {t}\n"
+                "Paired: {p}".format(
+                    d=i['identifier'],
+                    m=i['model'],
+                    a=i['product'],
+                    s=i['status'],
+                    t=i['transport_id'],
+                    p=paired
+                )
+            )
+
+            devices_view_list_item.setFont(QFont('Noto Sans', pointSize=8))
+            log(device_paired_and_exists)
+            if device_paired_and_exists:
+                continue
+            # If and only if the device doesn't exist; add it
+            self.devices_view.addItem(devices_view_list_item)
+        return devices
     def __dimension_change_cb(self):
         if self.dimensionDefaultCheckbox.isChecked():
             self.dimensionSlider.setEnabled(False)

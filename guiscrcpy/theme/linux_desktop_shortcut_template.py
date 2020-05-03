055f1f54bb677cbe0a1d0b7fe9c81b45bbb3f3df
Srevin Saju <srevin03@gmail.com>
2020-05-03 21:52:40 +0300

    Add desktop file template and icon #112 #113

---
+++ guiscrcpy/theme/linux_desktop_shortcut_template.py
@@ -0,0 +1,11 @@
+GUISCRCPY_DEVICE = \
+    """[Desktop Entry]
+Name={identifier}
+GenericName=guiscrcpy
+Comment=Open Source Android Screen Mirroring System
+Exec={command}
+Icon={icon_path}
+Type=Application
+Categories=Utility;Development;
+StartupWMClass=UGENE
+"""

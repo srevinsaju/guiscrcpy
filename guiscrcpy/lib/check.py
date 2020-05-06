c93252184b61967558620299b7aa66c475389487
Srevin Saju <srevin03@gmail.com>
2020-05-06 22:50:41 +0300

    Fix udev errors on guiscrcpy adb connect due to bad adb connection #117

--- guiscrcpy/lib/check.py
+++ guiscrcpy/lib/check.py
@@ -174,9 +174,18 @@ class adb:
                   for x in decode_process(proc)[1:]][:-1]
         devices_found = []
         for device in output:
+            # https://github.com/srevinsaju/guiscrcpy/issues/117
+            if 'udev' in device and 'permission' in device:
+                # This is an error with some linux and Windows OSes
+                # This happens because the udev is not configured
+                # and linux adb does not have access to reading the device
+                # the status hence should be 'no_permission'
+                status = 'no_permission'
+            else:
+                status = device[1]
             description = {
                 'identifier': device[0],
-                'status': device[1],
+                'status': status,
                 'product': get(device, 2, ':').split(':')[-1],
                 'model': get(device, 3, ':').split(':')[-1],
                 'device': get(device, 4, ':').split(':')[-1],

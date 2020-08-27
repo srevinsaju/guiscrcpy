1ff3f6ca445ec781173463eba52f7d3b83c287eb
Srevin Saju <srevinsaju@sugarlabs.org>
2020-08-27 18:33:56 +0300

    Remove adb.shell path parameter

--- guiscrcpy/lib/toolkit.py
+++ guiscrcpy/lib/toolkit.py
@@ -142,13 +142,12 @@ class UXMapper:
 
     def reorient_landscape(self):
         logging.debug("Passing REORIENT [LANDSCAPE]")
-        adb.shell(
-            adb.path,
+        self.adb.shell(
             'settings put system accelerometer_rotation 0',
             device_id=self.deviceId
         )
-        adb.shell(
-            adb.path, "settings put system rotation 1",
+        self.adb.shell(
+            "settings put system rotation 1",
             device_id=self.deviceId)
 
     def expand_notifications(self):

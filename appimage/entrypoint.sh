35acead4df99be1942de7ebb2580c92d5a259130
Srevin Saju <srevinsaju@sugarlabs.org>
2020-09-01 16:16:40 +0300

    Use scrcpy-appimage to build guiscrcpy appimage

--- appimage/entrypoint.sh
+++ appimage/entrypoint.sh
@@ -1,9 +1,8 @@
-export LD_LIBRARY_PATH="${APPDIR}/usr/lib:${LD_LIBRARY_PATH}"
-export GUISCRCPY_SCRCPY="${APPDIR}/usr/bin/scrcpy"
-export GUISCRCPY_ADB="${APPDIR}/usr/bin/adb"
-export ADB="${APPDIR}/usr/bin/adb"
-export SCRCPY_SERVER_PATH="${APPDIR}/usr/share/scrcpy/scrcpy-server"
-export PATH="${APPDIR}/usr/bin:${PATH}"
-export GUISCRCPY_APPIMAGE="TRUE"
+export GUISCRCPY_SCRCPY=${APPDIR}/usr/bin/scrcpy
+export GUISCRCPY_ADB=${APPDIR}/usr/bin/adb
+export SCRCPY_SERVER_PATH=${APPDIR}/usr/local/share/scrcpy/scrcpy-server
+export ADB=${APPDIR}/usr/bin/adb
+export GUISCRCPY_APPIMAGE=True
+export PATH=${APPDIR}/usr/bin:${PATH}
 
 {{ python-executable }} -s ${APPDIR}/opt/python{{ python-version }}/bin/guiscrcpy "$@"

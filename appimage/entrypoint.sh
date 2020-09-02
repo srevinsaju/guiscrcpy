843de2fa5e16259fe8f7388013644daa3a17bbe5
Srevin Saju <srevinsaju@sugarlabs.org>
2020-09-02 17:12:06 +0300

    Fix exec error on using AppImages in directories containing spaces

--- appimage/entrypoint.sh
+++ appimage/entrypoint.sh
@@ -1,8 +1,8 @@
-export GUISCRCPY_SCRCPY=${APPDIR}/usr/bin/scrcpy
-export GUISCRCPY_ADB=${APPDIR}/usr/bin/adb
-export SCRCPY_SERVER_PATH=${APPDIR}/usr/local/share/scrcpy/scrcpy-server
-export ADB=${APPDIR}/usr/bin/adb
+export GUISCRCPY_SCRCPY="${APPDIR}/usr/bin/scrcpy"
+export GUISCRCPY_ADB="${APPDIR}/usr/bin/adb"
+export SCRCPY_SERVER_PATH="${APPDIR}/usr/local/share/scrcpy/scrcpy-server"
+export ADB="${APPDIR}/usr/bin/adb"
 export GUISCRCPY_APPIMAGE=True
-export PATH=${APPDIR}/usr/bin:${PATH}
+export PATH="${APPDIR}/usr/bin:${PATH}"
 
-{{ python-executable }} -s ${APPDIR}/opt/python{{ python-version }}/bin/guiscrcpy "$@"
+"{{ python-executable }}" -s "${APPDIR}/opt/python{{ python-version }}/bin/guiscrcpy" "$@"

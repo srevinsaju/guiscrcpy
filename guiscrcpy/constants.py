fa9469e60a4b962057da83a176eee0fcb28d9056
Srevin Saju <srevinsaju@sugarlabs.org>
2020-09-02 17:10:13 +0300

    Add a FONTS constant

---
+++ guiscrcpy/constants.py
@@ -0,0 +1,8 @@
+FONTS = [
+    'NotoSans-BoldItalic.ttf',
+    'NotoSans-Italic.ttf',
+    'TitilliumWeb-Bold.ttf',
+    'NotoSans-Bold.ttf',
+    'NotoSans-Regular.ttf',
+    'TitilliumWeb-Regular.ttf',
+]

fc10821d27e288a082ebe136ec4ee83873771c8c
Srevin Saju <srevinsaju@sugarlabs.org>
2020-09-24 18:32:22 +0300

    chore: release v4.7.3 :tada:

--- setup.py
+++ setup.py
@@ -49,7 +49,7 @@ data_files = [
 
 setup(
     name='guiscrcpy',
-    version='v4.7.2.post3.dev',
+    version='v4.7.3',
     description='An Open Source - Fast - Android Screen Mirroring system.',
     long_description=long_description,
     long_description_content_type='text/markdown',

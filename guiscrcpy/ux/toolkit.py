4db4c2cc6be67d8b6ad210c86bf7a6b6017b48c8
Restyled.io <commits@restyled.io>
2020-09-24 14:14:19 +0000

    Restyled by reorder-python-imports

--- guiscrcpy/ux/toolkit.py
+++ guiscrcpy/ux/toolkit.py
@@ -16,7 +16,6 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """
-
 import platform
 import shutil
 import uuid

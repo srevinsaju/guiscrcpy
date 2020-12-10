907f40644243a855e86b3547b8b38101403e3c2a
Srevin Saju <srevinsaju@sugarlabs.org>
2020-12-10 23:40:20 +0300

    chore: update version

--- guiscrcpy/version.py
+++ guiscrcpy/version.py
@@ -20,4 +20,4 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 """
 
-VERSION = "4.0.x.src.dev"
+VERSION = "v4.7.5"
--- setup.py
+++ setup.py
@@ -49,7 +49,7 @@ data_files = [
 
 setup(
     name="guiscrcpy",
-    version="4.0.x.src.dev",
+    version="v4.7.5",
     description="An Open Source - Fast - Android Screen Mirroring system.",
     long_description=long_description,
     long_description_content_type='text/markdown',

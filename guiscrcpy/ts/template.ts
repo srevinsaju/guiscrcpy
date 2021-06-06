b0f1d86468bba7487baeab20f4a5a362c3ee5b51
Srevin Saju <srevinsaju@sugarlabs.org>
2021-06-06 12:45:28 +0300

    chore: add template.ts for translations

---
+++ guiscrcpy/ts/template.ts
@@ -0,0 +1,492 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS><TS version="2.0">
+<context>
+    <name>HorizontalPanel</name>
+    <message>
+        <location filename="../ui/pyside2/panel.py" line="137"/>
+        <source>guiscrcpy</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/pyside2/panel.py" line="139"/>
+        <source>Back key</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/pyside2/panel.py" line="143"/>
+        <source>Power on/off</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/pyside2/panel.py" line="147"/>
+        <source>Menu key</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/pyside2/panel.py" line="151"/>
+        <source>Volume Up</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/pyside2/panel.py" line="155"/>
+        <source>Home key</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/pyside2/panel.py" line="159"/>
+        <source>::</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>Initializer</name>
+    <message>
+        <location filename="../ui/pyside2/downloader.py" line="74"/>
+        <source>Initializing</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/pyside2/downloader.py" line="73"/>
+        <source>guiscrcpy</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../ui/pyside2/main.py" line="764"/>
+        <source>guiscrcpy</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/pyside2/main.py" line="766"/>
+        <source>GUI by srevinsaju, scrcpy engine by rom1v. Hosted on GitHub
+                https://github.com/srevinsaju/guiscrcpy
+            </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/pyside2/main.py" line="770"/>
+        <source>Fullscreen</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/pyside2/main.py" line="772"/>
+        <source>Keep display always on top of other windows</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/pyside2/settings.py" line="209"/>
+        <source>Always on Top</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/pyside2/main.py" line="776"/>
+        <source>Swipe Panel</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/pyside2/main.py" line="777"/>
+        <source>Show touches</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/pyside2/main.py" line="779"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Record your screen mirroring to your home directory. In Linux, it is in ~guiscrcpy directory. In Windows, it is in C:\Users\&amp;lt;username&amp;gt;\ with the UNIX date, followed by .mp4; For manual configuration and path selection, use the settings icon&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/pyside2/main.py" line="782"/>
+        <source>See tooltip</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/pyside2/main.py" line="784"/>
+        <source>Record screen</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/pyside2/main.py" line="786"/>
+        <source>Use your computer screen as your android device&apos;s HD Display</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/pyside2/main.py" line="788"/>
+        <source>Keep display off </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/pyside2/main.py" line="789"/>
+        <source>Lock Rotation</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/pyside2/main.py" line="790"/>
+        <source>Bottom Panel</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/pyside2/main.py" line="792"/>
+        <source>Launch Network Manager</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/pyside2/main.py" line="795"/>
+        <source>Side Panel</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/pyside2/main.py" line="797"/>
+        <source>Default Rotation</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/pyside2/main.py" line="798"/>
+        <source>Potrait</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/pyside2/main.py" line="799"/>
+        <source>Landscape</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/pyside2/main.py" line="800"/>
+        <source>Potrait (flipped)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/pyside2/main.py" line="801"/>
+        <source>Landscape (flipped)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/pyside2/main.py" line="804"/>
+        <source>Restart the adb-server</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/pyside2/main.py" line="811"/>
+        <source>Nothing here</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/pyside2/main.py" line="815"/>
+        <source>Keep Default Dimensions</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/pyside2/main.py" line="816"/>
+        <source>Bitrate</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/pyside2/main.py" line="817"/>
+        <source>00000 A\B/s</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/pyside2/main.py" line="818"/>
+        <source>8000 KB/s</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/pyside2/main.py" line="820"/>
+        <source>Changing the bitrate of the screen mirrorring. Useful to show playback at normal speed without lags</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/pyside2/main.py" line="823"/>
+        <source>Adjust Bitrate. Leave it untouched for defualts</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/pyside2/main.py" line="827"/>
+        <source>DEFAULT</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/pyside2/main.py" line="828"/>
+        <source>Enter additional flags to pass to scrcpy</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/pyside2/main.py" line="830"/>
+        <source>Quit GUI and end all subprocess windows </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/pyside2/main.py" line="834"/>
+        <source>Reset guiscrcpy configuration files</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/pyside2/main.py" line="836"/>
+        <source>  RESET  </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/pyside2/main.py" line="838"/>
+        <source>Check me out on Github</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/pyside2/main.py" line="842"/>
+        <source>Start @rom1v&apos;s usbaudio. Needs usbaudio to be on PATH</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/pyside2/main.py" line="849"/>
+        <source>Initialize mapper</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/pyside2/main.py" line="853"/>
+        <source>Start Scrcpy Executable right now. Please check if Scrcpy is added to path</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/pyside2/main.py" line="855"/>
+        <source>START SCRCPY</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/pyside2/main.py" line="857"/>
+        <source>Progress Bar</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/pyside2/main.py" line="871"/>
+        <source>GUI by srevinsaju, scrcpy engine by rom1v. Hosted on GitHub
+                        https://github.com/srevinsaju/guiscrcpy
+                    </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/pyside2/main.py" line="875"/>
+        <source>guiscrcpy                          </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/pyside2/main.py" line="876"/>
+        <source>Build</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/pyside2/settings.py" line="206"/>
+        <source>Settings</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/pyside2/settings.py" line="207"/>
+        <source>h</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/pyside2/settings.py" line="208"/>
+        <source>w</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/pyside2/settings.py" line="220"/>
+        <source>y</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/pyside2/settings.py" line="211"/>
+        <source>Prefer Text</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/pyside2/settings.py" line="212"/>
+        <source>...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/pyside2/settings.py" line="213"/>
+        <source>Max Frame / second (FPS)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/pyside2/settings.py" line="216"/>
+        <source>x</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/pyside2/settings.py" line="215"/>
+        <source>Borderless Window</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/pyside2/settings.py" line="217"/>
+        <source>Update</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/pyside2/settings.py" line="218"/>
+        <source>Record File</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/pyside2/settings.py" line="219"/>
+        <source>Serial</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/pyside2/settings.py" line="221"/>
+        <source>Window position</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/pyside2/settings.py" line="222"/>
+        <source>Window size</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/pyside2/settings.py" line="223"/>
+        <source>Crop Width</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/pyside2/settings.py" line="224"/>
+        <source>Window Title</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/pyside2/settings.py" line="225"/>
+        <source>Push Target</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>NetworkUI</name>
+    <message>
+        <location filename="../ui/pyside2/network.py" line="98"/>
+        <source>Network Manager</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/pyside2/network.py" line="99"/>
+        <source>List of Network Devices</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/pyside2/network.py" line="101"/>
+        <source>Refresh Devices list</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/pyside2/network.py" line="103"/>
+        <source>REFRESH</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/pyside2/network.py" line="104"/>
+        <source>TCPIP</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/pyside2/network.py" line="109"/>
+        <source>Port number</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/pyside2/network.py" line="112"/>
+        <source>Establish connection</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/pyside2/network.py" line="114"/>
+        <source>CONNECT</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/pyside2/network.py" line="116"/>
+        <source>Cannot find your IP address? Enter it manually here</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>ToolbarPanel</name>
+    <message>
+        <location filename="../ui/pyside2/toolkit.py" line="384"/>
+        <source>Fullscreen</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/pyside2/toolkit.py" line="392"/>
+        <source>Expand notification panel</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/pyside2/toolkit.py" line="396"/>
+        <source>Copy device clipbioard to PC</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/pyside2/toolkit.py" line="400"/>
+        <source>Copy PC clipboard to Device</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/pyside2/toolkit.py" line="404"/>
+        <source>Volume Up</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/pyside2/toolkit.py" line="409"/>
+        <source>Power on/off</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/pyside2/toolkit.py" line="413"/>
+        <source>Home key</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/pyside2/toolkit.py" line="417"/>
+        <source>Back key</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/pyside2/toolkit.py" line="421"/>
+        <source>Menu key</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/pyside2/toolkit.py" line="425"/>
+        <source>press the APP_SWITCH button</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/pyside2/toolkit.py" line="429"/>
+        <source>Pinch in the screen</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/pyside2/toolkit.py" line="434"/>
+        <source>Pinch out in the screen</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/pyside2/toolkit.py" line="439"/>
+        <source>Potrait</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/pyside2/toolkit.py" line="449"/>
+        <source>Landscape</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/pyside2/toolkit.py" line="453"/>
+        <source>....</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/pyside2/toolkit.py" line="454"/>
+        <source>::::</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>

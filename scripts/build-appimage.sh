540a5aee6f322b97197ca820ead948b0a1ff9a6e
Srevin Saju <srevinsaju@sugarlabs.org>
2021-09-20 06:18:26 +0300

    ci: use /releases instead of /releases/latest for scrcpy-appimage

--- scripts/build-appimage.sh
+++ scripts/build-appimage.sh
@@ -8,13 +8,13 @@ wget -q https://github.com/AppImage/AppImageKit/releases/download/continuous/app
 
 
 if [[ "$GUISCRCPY_TYPE" == "r" ]]; then
-  SCRCPY_APPIMAGE_DOWNLOAD_URL="$(curl -sL https://api.github.com/repos/srevinsaju/scrcpy-appimage/releases/latest | jq -r '.assets[].browser_download_url' | grep -v 'zsync')"
+  SCRCPY_APPIMAGE_DOWNLOAD_URL="$(curl -sL https://api.github.com/repos/srevinsaju/scrcpy-appimage/releases | jq -r '.[0].assets[].browser_download_url' | grep -v 'zsync')"
   export SCRCPY_APPIMAGE_DOWNLOAD_URL
 
   wget -q "$SCRCPY_APPIMAGE_DOWNLOAD_URL"
   chmod +x scrcpy*.AppImage
   ./scrcpy*.AppImage --appimage-extract
-  mv squashfs-root guiscrcpy.AppDir/scrcpy
+  mv squashfs-root/* guiscrcpy.AppDir/scrcpy/.
 
 else
   cp pyappimage/AppRun.standalone.sh guiscrcpy.AppDir/AppRun && chmod +x guiscrcpy.AppDir/AppRun
@@ -24,4 +24,4 @@ mkdir _build
 
 ./appimagetool-x86_64.AppImage guiscrcpy.AppDir -n \
   -u "gh-releases-zsync|srevinsaju|guiscrcpy|continuous|guiscrcpy-*.$GUISCRCPY_TYPE.*.AppImage.zsync" \
-  "_build/guiscrcpy-min-$GUISCRCPY_VERSION.$GUISCRCPY_TYPE.$GUISCRCPY_GLIB_VERSION-$GUISCRCPY_MACHINE.AppImage"
+  "_build/guiscrcpy-$GUISCRCPY_VERSION.$GUISCRCPY_TYPE.$GUISCRCPY_GLIB_VERSION-$GUISCRCPY_MACHINE.AppImage"

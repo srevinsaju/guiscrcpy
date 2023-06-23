077adacb3749bd75b72debc644b53a48e3edac51
SummonHIM <2239760702@qq.com>
2023-06-24 03:40:38 +0800

    Upload chinese translate

---
+++ guiscrcpy/ts/eng-chs.ts
@@ -0,0 +1,495 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="zh_CN" sourcelanguage="zh_CN">
+<context>
+    <name>HorizontalPanel</name>
+    <message>
+        <location filename="../ui/pyside2/panel.py" line="137"/>
+        <source>guiscrcpy</source>
+        <translation>guiscrcpy</translation>
+    </message>
+    <message>
+        <location filename="../ui/pyside2/panel.py" line="139"/>
+        <source>Back key</source>
+        <translation>返回键</translation>
+    </message>
+    <message>
+        <location filename="../ui/pyside2/panel.py" line="143"/>
+        <source>Power on/off</source>
+        <translation>电源键</translation>
+    </message>
+    <message>
+        <location filename="../ui/pyside2/panel.py" line="147"/>
+        <source>Menu key</source>
+        <translation>菜单键</translation>
+    </message>
+    <message>
+        <location filename="../ui/pyside2/panel.py" line="151"/>
+        <source>Volume Up</source>
+        <translation>音量加</translation>
+    </message>
+    <message>
+        <location filename="../ui/pyside2/panel.py" line="155"/>
+        <source>Home key</source>
+        <translation>主界面</translation>
+    </message>
+    <message>
+        <location filename="../ui/pyside2/panel.py" line="159"/>
+        <source>::</source>
+        <translation>::</translation>
+    </message>
+</context>
+<context>
+    <name>Initializer</name>
+    <message>
+        <location filename="../ui/pyside2/downloader.py" line="74"/>
+        <source>Initializing</source>
+        <translation>正在初始化</translation>
+    </message>
+    <message>
+        <location filename="../ui/pyside2/downloader.py" line="73"/>
+        <source>guiscrcpy</source>
+        <translation>guiscrcpy</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../ui/pyside2/main.py" line="764"/>
+        <source>guiscrcpy</source>
+        <translation>guiscrcpy</translation>
+    </message>
+    <message>
+        <location filename="../ui/pyside2/main.py" line="766"/>
+        <source>GUI by srevinsaju, scrcpy engine by rom1v. Hosted on GitHub
+                https://github.com/srevinsaju/guiscrcpy
+            </source>
+        <translation>本项目由 srevinsaju 制作的图形化界面，rom1v 制作的 scrcpy 引擎。并在 GitHub 上开源
+                https://github.com/srevinsaju/guiscrcpy
+            </translation>
+    </message>
+    <message>
+        <location filename="../ui/pyside2/main.py" line="770"/>
+        <source>Fullscreen</source>
+        <translation>全屏</translation>
+    </message>
+    <message>
+        <location filename="../ui/pyside2/main.py" line="772"/>
+        <source>Keep display always on top of other windows</source>
+        <translation>始终将窗口置于最顶上</translation>
+    </message>
+    <message>
+        <location filename="../ui/pyside2/settings.py" line="209"/>
+        <source>Always on Top</source>
+        <translation>窗口置顶</translation>
+    </message>
+    <message>
+        <location filename="../ui/pyside2/main.py" line="776"/>
+        <source>Swipe Panel</source>
+        <translation>滑动面板</translation>
+    </message>
+    <message>
+        <location filename="../ui/pyside2/main.py" line="777"/>
+        <source>Show touches</source>
+        <translation>显示触摸位置</translation>
+    </message>
+    <message>
+        <location filename="../ui/pyside2/main.py" line="779"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Record your screen mirroring to your home directory. In Linux, it is in ~guiscrcpy directory. In Windows, it is in C:\Users\&amp;lt;username&amp;gt;\ with the UNIX date, followed by .mp4; For manual configuration and path selection, use the settings icon&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;录制当前镜像屏幕并保存至您的主目录。Linux 系统将保存至 ~guiscrcpy 文件夹。Windows 系统将保存至 C:\Users\&amp;lt;username&amp;gt;\ 文件夹，后缀为 .mp4，并以 UNIX 时间保存。; 如需手动配置和另存路径，请前去设置页&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+    </message>
+    <message>
+        <location filename="../ui/pyside2/main.py" line="782"/>
+        <source>See tooltip</source>
+        <translation>查看工具提示</translation>
+    </message>
+    <message>
+        <location filename="../ui/pyside2/main.py" line="784"/>
+        <source>Record screen</source>
+        <translation>录制屏幕</translation>
+    </message>
+    <message>
+        <location filename="../ui/pyside2/main.py" line="786"/>
+        <source>Use your computer screen as your android device&apos;s HD Display</source>
+        <translation>使用电脑的屏幕来作为您当前安卓设备的高清显示屏</translation>
+    </message>
+    <message>
+        <location filename="../ui/pyside2/main.py" line="788"/>
+        <source>Keep display off </source>
+        <translation>保持屏幕关闭</translation>
+    </message>
+    <message>
+        <location filename="../ui/pyside2/main.py" line="789"/>
+        <source>Lock Rotation</source>
+        <translation>方向锁定</translation>
+    </message>
+    <message>
+        <location filename="../ui/pyside2/main.py" line="790"/>
+        <source>Bottom Panel</source>
+        <translation>底部面板</translation>
+    </message>
+    <message>
+        <location filename="../ui/pyside2/main.py" line="792"/>
+        <source>Launch Network Manager</source>
+        <translation>启动网络管理器</translation>
+    </message>
+    <message>
+        <location filename="../ui/pyside2/main.py" line="795"/>
+        <source>Side Panel</source>
+        <translation>侧边面板</translation>
+    </message>
+    <message>
+        <location filename="../ui/pyside2/main.py" line="797"/>
+        <source>Default Rotation</source>
+        <translation>默认方向</translation>
+    </message>
+    <message>
+        <location filename="../ui/pyside2/main.py" line="798"/>
+        <source>Potrait</source>
+        <translation>竖屏</translation>
+    </message>
+    <message>
+        <location filename="../ui/pyside2/main.py" line="799"/>
+        <source>Landscape</source>
+        <translation>横屏</translation>
+    </message>
+    <message>
+        <location filename="../ui/pyside2/main.py" line="800"/>
+        <source>Potrait (flipped)</source>
+        <translation>竖屏（反转）</translation>
+    </message>
+    <message>
+        <location filename="../ui/pyside2/main.py" line="801"/>
+        <source>Landscape (flipped)</source>
+        <translation>横屏（反转）</translation>
+    </message>
+    <message>
+        <location filename="../ui/pyside2/main.py" line="804"/>
+        <source>Restart the adb-server</source>
+        <translation>重启 ADB 服务端</translation>
+    </message>
+    <message>
+        <location filename="../ui/pyside2/main.py" line="811"/>
+        <source>Nothing here</source>
+        <translation>这里啥都没有</translation>
+    </message>
+    <message>
+        <location filename="../ui/pyside2/main.py" line="815"/>
+        <source>Keep Default Dimensions</source>
+        <translation>保持默认尺寸</translation>
+    </message>
+    <message>
+        <location filename="../ui/pyside2/main.py" line="816"/>
+        <source>Bitrate</source>
+        <translation>比特率</translation>
+    </message>
+    <message>
+        <location filename="../ui/pyside2/main.py" line="817"/>
+        <source>00000 A\B/s</source>
+        <translation>00000 A\B/s</translation>
+    </message>
+    <message>
+        <location filename="../ui/pyside2/main.py" line="818"/>
+        <source>8000 KB/s</source>
+        <translation>8000 KB/s</translation>
+    </message>
+    <message>
+        <location filename="../ui/pyside2/main.py" line="820"/>
+        <source>Changing the bitrate of the screen mirrorring. Useful to show playback at normal speed without lags</source>
+        <translation>变更屏幕镜像的比特率。过低值可能显示更模糊，过高值可能运行更卡顿。</translation>
+    </message>
+    <message>
+        <location filename="../ui/pyside2/main.py" line="823"/>
+        <source>Adjust Bitrate. Leave it untouched for defualts</source>
+        <translation>调整比特率。若不调整则为保持默认</translation>
+    </message>
+    <message>
+        <location filename="../ui/pyside2/main.py" line="827"/>
+        <source>DEFAULT</source>
+        <translation>默认</translation>
+    </message>
+    <message>
+        <location filename="../ui/pyside2/main.py" line="828"/>
+        <source>Enter additional flags to pass to scrcpy</source>
+        <translation>为 scrcpy 传递自定义参数。</translation>
+    </message>
+    <message>
+        <location filename="../ui/pyside2/main.py" line="830"/>
+        <source>Quit GUI and end all subprocess windows </source>
+        <translation>退出本界面并退出所有子窗口。</translation>
+    </message>
+    <message>
+        <location filename="../ui/pyside2/main.py" line="834"/>
+        <source>Reset guiscrcpy configuration files</source>
+        <translation>重置 guiscrcpy 配置文件</translation>
+    </message>
+    <message>
+        <location filename="../ui/pyside2/main.py" line="836"/>
+        <source>  RESET  </source>
+        <translation>  重置  </translation>
+    </message>
+    <message>
+        <location filename="../ui/pyside2/main.py" line="838"/>
+        <source>Check me out on Github</source>
+        <translation>欢迎访问本项目 Github</translation>
+    </message>
+    <message>
+        <location filename="../ui/pyside2/main.py" line="842"/>
+        <source>Start @rom1v&apos;s usbaudio. Needs usbaudio to be on PATH</source>
+        <translation>启动 @rom1v 制作的 usbaudio（USB音频）。需要将 usbaudio 所在文件夹添加至 Path 变量。</translation>
+    </message>
+    <message>
+        <location filename="../ui/pyside2/main.py" line="849"/>
+        <source>Initialize mapper</source>
+        <translation>初始化映射器</translation>
+    </message>
+    <message>
+        <location filename="../ui/pyside2/main.py" line="853"/>
+        <source>Start Scrcpy Executable right now. Please check if Scrcpy is added to path</source>
+        <translation>启动 Scrcpy 可执行文件。请检查 Scrcpy 所在文件夹是否已添加至 Path 变量。</translation>
+    </message>
+    <message>
+        <location filename="../ui/pyside2/main.py" line="855"/>
+        <source>START SCRCPY</source>
+        <translation>启动 SCRCPY</translation>
+    </message>
+    <message>
+        <location filename="../ui/pyside2/main.py" line="857"/>
+        <source>Progress Bar</source>
+        <translation>进度条</translation>
+    </message>
+    <message>
+        <location filename="../ui/pyside2/main.py" line="871"/>
+        <source>GUI by srevinsaju, scrcpy engine by rom1v. Hosted on GitHub
+                        https://github.com/srevinsaju/guiscrcpy
+                    </source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../ui/pyside2/main.py" line="875"/>
+        <source>guiscrcpy                          </source>
+        <translation>guiscrcpy                          </translation>
+    </message>
+    <message>
+        <location filename="../ui/pyside2/main.py" line="876"/>
+        <source>Build</source>
+        <translation>版本</translation>
+    </message>
+    <message>
+        <location filename="../ui/pyside2/settings.py" line="206"/>
+        <source>Settings</source>
+        <translation>设置</translation>
+    </message>
+    <message>
+        <location filename="../ui/pyside2/settings.py" line="207"/>
+        <source>h</source>
+        <translation>h</translation>
+    </message>
+    <message>
+        <location filename="../ui/pyside2/settings.py" line="208"/>
+        <source>w</source>
+        <translation>w</translation>
+    </message>
+    <message>
+        <location filename="../ui/pyside2/settings.py" line="220"/>
+        <source>y</source>
+        <translation>y</translation>
+    </message>
+    <message>
+        <location filename="../ui/pyside2/settings.py" line="211"/>
+        <source>Prefer Text</source>
+        <translation>文本优先</translation>
+    </message>
+    <message>
+        <location filename="../ui/pyside2/settings.py" line="212"/>
+        <source>...</source>
+        <translation>...</translation>
+    </message>
+    <message>
+        <location filename="../ui/pyside2/settings.py" line="213"/>
+        <source>Max Frame / second (FPS)</source>
+        <translation>每秒最大帧率 (FPS)</translation>
+    </message>
+    <message>
+        <location filename="../ui/pyside2/settings.py" line="216"/>
+        <source>x</source>
+        <translation>x</translation>
+    </message>
+    <message>
+        <location filename="../ui/pyside2/settings.py" line="215"/>
+        <source>Borderless Window</source>
+        <translation>无边框窗口</translation>
+    </message>
+    <message>
+        <location filename="../ui/pyside2/settings.py" line="217"/>
+        <source>Update</source>
+        <translation>更新配置</translation>
+    </message>
+    <message>
+        <location filename="../ui/pyside2/settings.py" line="218"/>
+        <source>Record File</source>
+        <translation>录制文件</translation>
+    </message>
+    <message>
+        <location filename="../ui/pyside2/settings.py" line="219"/>
+        <source>Serial</source>
+        <translation>序列号</translation>
+    </message>
+    <message>
+        <location filename="../ui/pyside2/settings.py" line="221"/>
+        <source>Window position</source>
+        <translation>窗口位置</translation>
+    </message>
+    <message>
+        <location filename="../ui/pyside2/settings.py" line="222"/>
+        <source>Window size</source>
+        <translation>窗口大小</translation>
+    </message>
+    <message>
+        <location filename="../ui/pyside2/settings.py" line="223"/>
+        <source>Crop Width</source>
+        <translation>裁剪宽度</translation>
+    </message>
+    <message>
+        <location filename="../ui/pyside2/settings.py" line="224"/>
+        <source>Window Title</source>
+        <translation>窗口标题</translation>
+    </message>
+    <message>
+        <location filename="../ui/pyside2/settings.py" line="225"/>
+        <source>Push Target</source>
+        <translation>推送目标</translation>
+    </message>
+</context>
+<context>
+    <name>NetworkUI</name>
+    <message>
+        <location filename="../ui/pyside2/network.py" line="98"/>
+        <source>Network Manager</source>
+        <translation>网络管理器</translation>
+    </message>
+    <message>
+        <location filename="../ui/pyside2/network.py" line="99"/>
+        <source>List of Network Devices</source>
+        <translation>网络设备列表</translation>
+    </message>
+    <message>
+        <location filename="../ui/pyside2/network.py" line="101"/>
+        <source>Refresh Devices list</source>
+        <translation>刷新设备列表</translation>
+    </message>
+    <message>
+        <location filename="../ui/pyside2/network.py" line="103"/>
+        <source>REFRESH</source>
+        <translation>刷新</translation>
+    </message>
+    <message>
+        <location filename="../ui/pyside2/network.py" line="104"/>
+        <source>TCPIP</source>
+        <translation>TCPIP</translation>
+    </message>
+    <message>
+        <location filename="../ui/pyside2/network.py" line="109"/>
+        <source>Port number</source>
+        <translation>端口号</translation>
+    </message>
+    <message>
+        <location filename="../ui/pyside2/network.py" line="112"/>
+        <source>Establish connection</source>
+        <translation>建立连接</translation>
+    </message>
+    <message>
+        <location filename="../ui/pyside2/network.py" line="114"/>
+        <source>CONNECT</source>
+        <translation>连接</translation>
+    </message>
+    <message>
+        <location filename="../ui/pyside2/network.py" line="116"/>
+        <source>Cannot find your IP address? Enter it manually here</source>
+        <translation>无法找到您的 IP 地址？可在此手动输入</translation>
+    </message>
+</context>
+<context>
+    <name>ToolbarPanel</name>
+    <message>
+        <location filename="../ui/pyside2/toolkit.py" line="384"/>
+        <source>Fullscreen</source>
+        <translation>全屏</translation>
+    </message>
+    <message>
+        <location filename="../ui/pyside2/toolkit.py" line="392"/>
+        <source>Expand notification panel</source>
+        <translation>展开通知面板</translation>
+    </message>
+    <message>
+        <location filename="../ui/pyside2/toolkit.py" line="396"/>
+        <source>Copy device clipbioard to PC</source>
+        <translation>复制剪切版内容至电脑</translation>
+    </message>
+    <message>
+        <location filename="../ui/pyside2/toolkit.py" line="400"/>
+        <source>Copy PC clipboard to Device</source>
+        <translation>复制电脑剪切板内容至设备</translation>
+    </message>
+    <message>
+        <location filename="../ui/pyside2/toolkit.py" line="404"/>
+        <source>Volume Up</source>
+        <translation>音量加</translation>
+    </message>
+    <message>
+        <location filename="../ui/pyside2/toolkit.py" line="409"/>
+        <source>Power on/off</source>
+        <translation>电源键</translation>
+    </message>
+    <message>
+        <location filename="../ui/pyside2/toolkit.py" line="413"/>
+        <source>Home key</source>
+        <translation>主界面</translation>
+    </message>
+    <message>
+        <location filename="../ui/pyside2/toolkit.py" line="417"/>
+        <source>Back key</source>
+        <translation>返回键</translation>
+    </message>
+    <message>
+        <location filename="../ui/pyside2/toolkit.py" line="421"/>
+        <source>Menu key</source>
+        <translation>菜单键</translation>
+    </message>
+    <message>
+        <location filename="../ui/pyside2/toolkit.py" line="425"/>
+        <source>press the APP_SWITCH button</source>
+        <translation>单击 APP_SWITCH 按钮</translation>
+    </message>
+    <message>
+        <location filename="../ui/pyside2/toolkit.py" line="429"/>
+        <source>Pinch in the screen</source>
+        <translation>屏幕放大（双指张开）</translation>
+    </message>
+    <message>
+        <location filename="../ui/pyside2/toolkit.py" line="434"/>
+        <source>Pinch out in the screen</source>
+        <translation>屏幕缩小（双指夹紧）</translation>
+    </message>
+    <message>
+        <location filename="../ui/pyside2/toolkit.py" line="439"/>
+        <source>Potrait</source>
+        <translation>竖屏</translation>
+    </message>
+    <message>
+        <location filename="../ui/pyside2/toolkit.py" line="449"/>
+        <source>Landscape</source>
+        <translation>横屏</translation>
+    </message>
+    <message>
+        <location filename="../ui/pyside2/toolkit.py" line="453"/>
+        <source>....</source>
+        <translation>....</translation>
+    </message>
+    <message>
+        <location filename="../ui/pyside2/toolkit.py" line="454"/>
+        <source>::::</source>
+        <translation>::::</translation>
+    </message>
+</context>
+</TS>
